8d51b4618550f8b23bd92fe2aee28d6651753e44
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-11-02 20:03:10 +0530

    add operate function

--- calculator.js
+++ calculator.js
@@ -13,3 +13,18 @@ function getMul(num1, num2) {
 function getDiv(num1, num2) {
     return num1/num2;
 }
+
+function operate (input1, input2, operator){
+    if (operator === "+"){
+        return getAdd(input1, input2);    
+    }
+    if (operator === "-"){
+        return getSub(input1, input2);    
+    }
+    if (operator === "*"){
+        return getMul(input1, input2);    
+    }
+    if (operator === "/"){
+        return getDiv(input1, input2);    
+    }
+}
