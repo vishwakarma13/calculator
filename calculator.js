67a70b29c3c284637f7249b3d5fab6a8c4d7ceda
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-11-04 15:05:25 +0530

    add eventlistener to keys from num3 to num0

--- calculator.js
+++ calculator.js
@@ -34,6 +34,38 @@ let display = document.querySelector("#display"); //display is selected universa
 let numonekey = document.querySelector("#numonekey");
 numonekey.addEventListener("click", ()=> display.textContent = 1);
 
-//button click events key2
+//button click events for key2
 let numtwokey = document.querySelector("#numtwokey");
 numtwokey.addEventListener("click", ()=> display.textContent = 2);
+
+//button click events for key3
+let numthreekey = document.querySelector("#numthreekey");
+numthreekey.addEventListener("click", ()=> display.textContent = 3);
+
+//button click events for key4
+let numfourkey = document.querySelector("#numfourkey");
+numfourkey.addEventListener("click", ()=> display.textContent = 4);
+
+//button click events for key5
+let numfivekey = document.querySelector("#numfivekey");
+numfivekey.addEventListener("click", ()=> display.textContent = 5);
+
+//button click events for key6
+let numsixkey = document.querySelector("#numsixkey");
+numsixkey.addEventListener("click", ()=> display.textContent = 6);
+
+//button click events for key7
+let numsevenkey = document.querySelector("#numsevenkey");
+numsevenkey.addEventListener("click", ()=> display.textContent = 7);
+
+//button click events for key8
+let numeightkey = document.querySelector("#numeightkey");
+numeightkey.addEventListener("click", ()=> display.textContent = 8);
+
+//button click events for key9
+let numninekey = document.querySelector("#numninekey");
+numninekey.addEventListener("click", ()=> display.textContent = 9);
+
+//button click events for key0
+let numzerokey = document.querySelector("#numzerokey");
+numzerokey.addEventListener("click", ()=> display.textContent = 0);
