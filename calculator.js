2d7b33f79697c46d38e4a2d4c6eec340aacc2f5f
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-11-06 01:08:10 +0530

    refactor the code and remove unused lines

--- calculator.js
+++ calculator.js
@@ -1,34 +1,14 @@
-function getAdd(num1, num2) {
-    return num1 + num2;
-}
-
-function getSub(num1, num2) {
-    return num1 - num2;
-}
-
-function getMul(num1, num2) {
-    return num1*num2;
-}
-
-function getDiv(num1, num2) {
-    return num1/num2;
-}
+function getAdd(num1, num2) {return num1 + num2;}
+function getSub(num1, num2) {return num1 - num2;}
+function getMul(num1, num2) {return num1 * num2;}
+function getDiv(num1, num2) {return num1 / num2;}
 
 function operate (input1, input2, operator){
-    if (operator === "+"){
-        return getAdd(input1, input2);    
-    }
-    if (operator === "-"){
-        return getSub(input1, input2);    
-    }
-    if (operator === "*"){
-        return getMul(input1, input2);    
-    }
-    if (operator === "/"){
-        return getDiv(input1, input2);    
-    }
+    if (operator === "+"){return getAdd(input1, input2);}
+    if (operator === "-"){return getSub(input1, input2);}
+    if (operator === "*"){return getMul(input1, input2);}
+    if (operator === "/"){return getDiv(input1, input2);}
 }
-let display = document.querySelector("#display"); //display is selected universally
 let displaypara = document.querySelector("#displaypara");
 
 //button click events for key1
